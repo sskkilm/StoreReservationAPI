dd292ff23797ff26685b6348024e6ef8f704478d
sskkilm <sskkilm@naver.com>
2024-03-20 20:59:18 +0900

    Initial commit

---
+++ src/main/java/zerobase/storereservationapi/StoreReservationApiApplication.java
@@ -0,0 +1,13 @@
+package zerobase.storereservationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreReservationApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreReservationApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zerobase/storereservationapi/StoreReservationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package zerobase.storereservationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreReservationApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
